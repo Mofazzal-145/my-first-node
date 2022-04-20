847047f124f6852d9b3b2eaadfe00b22cff7ed45
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-20 22:26:48 +0600

    initialize node application

---
+++ _steps.js
@@ -0,0 +1,18 @@
+/* 
+* 1. create a folder
+  2. open command line to that folder
+  3. npm init
+  4. npm install express
+  5. create index.js
+    --------
+    For Express/node server
+    -------
+  1. require express
+  2. create app variable
+  3. declare port
+  4. app.get
+  5. app.listen
+  6. node index.js
+  7. check the browser for the port 
+
+ */
