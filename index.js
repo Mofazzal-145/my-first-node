8819df53e2704a257d06d1317e8d9372ae16ccfd
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-21 14:32:20 +0600

    start the sarver

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.port || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world with mofu');
@@ -17,6 +21,16 @@ const users = [
 ]
 
 app.get('/users', (req, res) => {
+    // filter by search query parameter
+    if(req.query.name){
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(matched);
+    }
+    else{
+        res.send(users);
+    }
+    console.log('query',req.query);
     res.send(users);
 })
 
@@ -26,8 +40,18 @@ app.get('/user/:id', (req, res) => {
     // const id = req.params.id;
     // const user = users[id];
     const user = users.find(u => u.id == id);
-    res.send(user)
+    res.send(user);
+});
+
+app.post('/user', (req, res) =>{
+    console.log('request',req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
 })
+
+
 
 app.get('/fruits', (req, res) => {
     res.send('mango', 'apples', 'oranges');
